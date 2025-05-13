a13ee9dfbb4d1f1255cf4b08c45d142bf9677028
miakojean <miakojeanyves@gmail.com>
2025-05-13 23:25:52 +0000

    13-05-25 (2) Joanny la dernière personne

---
+++ brouillon.py
@@ -0,0 +1,11 @@
+thematique="Audit et contrôle",
+domaine="Audit et contrôle",
+filiere="Audit et contrôle",
+
+thematique="économie du développement",
+domaine="économie du développement",
+filiere="économie du développement",
+
+thematique="Banque et assurance",
+domaine="Banque et assurance",
+filiere="Banque et assurance",
